2deb9638f9b7cac4e07d97d560c4a24447ea6c98
71M073J <tc7476@student.uni-lj.si>
2022-01-23 20:11:40 +0100

    ready

--- CustomTheory.js
+++ CustomTheory.js
@@ -1,4 +1,4 @@
-﻿﻿import { Cost, CustomCost, ExponentialCost, FreeCost, LinearCost, StepwiseCost } from "./api/Costs";
+﻿﻿import { ConstantCost, Cost, CustomCost, ExponentialCost, FreeCost, LinearCost, StepwiseCost } from "./api/Costs";
 import { Localization } from "./api/Localization";
 import { BigNumber } from "./api/BigNumber";
 import { theory } from "./api/Theory";
@@ -12,9 +12,9 @@ var version = 1.3;
 
 var currency;
 var t = 0.0;
-var f, c1, c2, q, q1, p, dt, tickCnt;
+var f, c1, c2, q, q1, p, dt, unbreak;
 var dtMilestone, c1Exp, c2Exp;
-var dts = [0.1, 0.05, 0.01]
+var dts = [0.1, 0.05, 0.025]
 var achievement1, achievement2;
 //var chapter1, chapter2;
 var maxt;
@@ -36,7 +36,6 @@ var init = () => {
     ///////////////////
     // Regular Upgrades
     //f, free upgrade
-    tickCnt = BigNumber.ZERO;
     {
         let getDesc = (level) => "f={" + getF(level) + "}";
         let getInfo = (level) => "f=" + getF(level);
@@ -53,6 +52,7 @@ var init = () => {
         q1.getDescription = (_) => Utils.getMath(getDesc(q1.level));
         q1.getInfo = (amount) => Utils.getMathTo(getInfo(q1.level), getInfo(q1.level + amount));
         q1.boughtOrRefunded = (_) => resetToPIMult();
+        q1.isAvailable = false
 
     }    
     // q2
@@ -64,6 +64,7 @@ var init = () => {
         q2.getDescription = (_) => Utils.getMath(getDesc(q2.level));
         q2.getInfo = (amount) => Utils.getMathTo(getInfo(q2.level), getInfo(q2.level + amount));
         q2.boughtOrRefunded = (_) => resetToPIMult();
+        q2.isAvailable = false
     }
     // p
     {
@@ -78,7 +79,7 @@ var init = () => {
     // c1
     {
         let getDesc = (level) => "c_1=" + getC1(level).toString(0);
-        c1 = theory.createUpgrade(2, currency, new ExponentialCost(15, Math.log2(2)));
+        c1 = theory.createUpgrade(2, currency, new ExponentialCost(1, Math.log10(5)));
         c1.getDescription = (_) => Utils.getMath(getDesc(c1.level));
         c1.getInfo = (amount) => Utils.getMathTo(getDesc(c1.level), getDesc(c1.level + amount));
         c1.boughtOrRefunded = (_) => resetToPIMult();
@@ -89,52 +90,60 @@ var init = () => {
     {
         let getDesc = (level) => "c_2=2^{" + level + "}";
         let getInfo = (level) => "c_2=" + getC2(level).toString(0);
-        c2 = theory.createUpgrade(3, currency, new ExponentialCost(1, 2));
+        c2 = theory.createUpgrade(3, currency, new ExponentialCost(50, 2));
         c2.getDescription = (_) => Utils.getMath(getDesc(c2.level));
         c2.getInfo = (amount) => Utils.getMathTo(getInfo(c2.level), getInfo(c2.level + amount));
         c2.boughtOrRefunded = (_) => resetToPIMult();
     }
 
+
     /////////////////////
     // Permanent Upgrades
     {
-        p1 = theory.createPublicationUpgrade(0, currency, 1e1);
+        p1 = theory.createPublicationUpgrade(0, currency, 1e10);
         p1.boughtOrRefunded = (_) => {resetToPIMult();};
     }
     {
-        p2 = theory.createBuyAllUpgrade(1, currency, 1e1);
+        p2 = theory.createBuyAllUpgrade(1, currency, 1e8);
         p1.boughtOrRefunded = (_) => resetToPIMult();
     }
     {
-        p3 = theory.createAutoBuyerUpgrade(2, currency, 1e1);
+        p3 = theory.createAutoBuyerUpgrade(2, currency, 1e13);
         p1.boughtOrRefunded = (_) => resetToPIMult();
+    }
+    {
+        unbreak = theory.createPermanentUpgrade(666, currency, new ConstantCost(0));
+        unbreak.getDescription = (_) => "Unfucks the gamestate, if stuck negative"
+        unbreak.getInfo = (_) => "Unfucks the gamestate, if stuck negative"
+        unbreak.boughtOrRefunded = (_) => {resetToPIMult(); t += Math.PI; currency.value = BigNumber.ZERO;}
     }
     ///////////////////////
     //// Milestone Upgrades
     // TODO make q variable a milestone 
     theory.setMilestoneCost(new LinearCost(25, 25));
-    theory.setMilestoneCost(new LinearCost(1, 1));
+    //theory.setMilestoneCost(new LinearCost(0, 0));
 
     {
-        dtMilestone = theory.createMilestoneUpgrade(0, 1);
-        dtMilestone.description = Localization.getUpgradeMultCustomDesc("dt", "0.2");
-        dtMilestone.info = Localization.getUpgradeMultCustomDesc("dt", "0.2");
+        dtMilestone = theory.createMilestoneUpgrade(0, 2);
+        dtMilestone.description = Localization.getUpgradeMultCustomDesc("dt", "0.5") + " " + Localization.getUpgradeMultCustomDesc("p", "+1");
+        dtMilestone.info = Localization.getUpgradeMultCustomInfo("dt", "0.5") + " " + Localization.getUpgradeMultCustomInfo("p", "+1");
         dtMilestone.boughtOrRefunded = (_) => {theory.invalidatePrimaryEquation(); resetToPIMult(); t = Math.sqrt(t)};
         dtMilestone.isAvailable = true;
     }
-    /*
+    
     {
-        c1Exp = theory.createMilestoneUpgrade(1, 3);
-        c1Exp.description = Localization.getUpgradeIncCustomExpDesc("c_1", "0.05");
-        c1Exp.info = Localization.getUpgradeIncCustomExpInfo("c_1", "0.05");
-        c1Exp.boughtOrRefunded = (_) => theory.invalidatePrimaryEquation();
+        qMilestone = theory.createMilestoneUpgrade(1, 1);
+        qMilestone.description = Localization.getUpgradeUnlockDesc("q_1");
+        qMilestone.info = Localization.getUpgradeUnlockInfo("q_2");
+        qMilestone.boughtOrRefunded = (_) => {theory.invalidatePrimaryEquation(); resetToPIMult(); updateAvailability();}
     }
+    
     {
-        c2Exp = theory.createMilestoneUpgrade(2, 3);
-        c2Exp.description = Localization.getUpgradeIncCustomExpDesc("c_2", "0.05");
-        c2Exp.info = Localization.getUpgradeIncCustomExpInfo("c_2", "0.05");
-        c2Exp.boughtOrRefunded = (_) => theory.invalidatePrimaryEquation();
-    }*/
+        qMilestone2 = theory.createMilestoneUpgrade(2, 1);
+        qMilestone2.description = Localization.getUpgradeUnlockDesc("q_2");
+        qMilestone2.info = Localization.getUpgradeUnlockInfo("q_2");
+        qMilestone2.boughtOrRefunded = (_) => {theory.invalidatePrimaryEquation(); resetToPIMult(); updateAvailability();}
+    }
     
     /////////////////
     //// Achievements
@@ -144,19 +153,20 @@ var init = () => {
     //updateAvailability();
 }
 
-//var updateAvailability = () => {
-//    c2Exp.isAvailable = c1Exp.level > 0;
-//}
+var updateAvailability = () => {
+    qMilestone2.isAvailable = qMilestone.level > 0;
+    q1.isAvailable = qMilestone.level > 0;
+    q2.isAvailable = qMilestone2.level > 0;
+}
 
 var tick = (elapsedTime, multiplier) => {
-    tickCnt += dts[dtMilestone.level];
     let dt = BigNumber.from(elapsedTime * multiplier);
     let bonus = theory.publicationMultiplier;
     currency.value += dt * 
         bonus * 
         q *
         (getF(f.level) + 
-        getC1(c1.level) * //.pow(getC1Exponent(c1Exp.level))
+        getC1(c1.level) *
         getC2(c2.level)) *
         (Math.pow(t, (dtMilestone.level + 1) *  getP(p.level)) /  (100*dts[dtMilestone.level])) *
         Math.cos(t);// - Math.sin(t) + Math.cos(t)) //.pow(getC2Exponent(c2Exp.level))
@@ -179,19 +189,13 @@ var getPrimaryEquation = () => {
     //if (c2Exp.level == 3) result += "^{0.15}";
     result += dtMilestone.level < 1 ? "\\frac{t" : "\\frac{t^" + (dtMilestone.level + 1)
     result += "^{p}"
-    result += "}{100dt} \\ q"
+    result += "}{100dt} \\ "
+    result += qMilestone.level > 0 ? "q" : ""
     result += "\\sin{(t)}"
     
     return result;
 }
-var trimSciNota = (n) => {
-    let temp = n.split("e");
-    if (temp[0][0] == "0"){
-        return "0";
-    } else{
-        return temp[0].substring(0,4) + "e" + temp[1];
-    }
-}
+
 var postPublish = () => {
     t = 0.0;
     maxt = 0.0;
@@ -203,16 +207,21 @@ var getSecondaryEquation = () => {
     result += "t={" + t.toFixed(2) + "}";
     result += "\\ ";
     result += "dt=" + dts[dtMilestone.level];
-    result += "\\\\";
-    result += "q={" + q + "}";
-    result += "\\ ";
-    result += "\\dot q={q_1q_2}/1000 ={" + (getQ1(q1.level) * getQ2(q2.level) / 1e3) + "}";
+    if(qMilestone.level > 0){
+        result += "\\\\";
+        result += "q={" + q + "}";
+        result += "\\ ";
+        result += "\\dot q={q_1" 
+        result += qMilestone2.level > 0 ? "q_2" : ""
+        result += "}/1000";
+    }
     result += "\\end{matrix}";
     return result
 }
 var getTertiaryEquation = () => theory.latexSymbol + "=\\max\\rho";
 var getPublicationMultiplier = (tau) => tau.pow(0.164) / BigNumber.THREE;
 var getPublicationMultiplierFormula = (symbol) => "\\frac{{" + symbol + "}^{0.164}}{3}";
+var getMilestone
 var getTau = () => currency.value;
 var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
 var getF = (level) => (level * 100)/1000
@@ -224,17 +233,17 @@ var getQ2 = (level) => BigNumber.THREE.pow(level);
 var getP = (level) => 1 + (level / 100) 
 var getC1Exponent = (level) => BigNumber.from(1 + 0.05 * level);
 var getC2Exponent = (level) => BigNumber.from(1 + 0.05 * level);
-//if you just need to seralise "t", here's an example: quoth philles gillipe
+//if you just need to serialise "t", here's an example: quoth philles gillipe
 var setInternalState = (state) => { //set the internal state of values that need to be kept post switch that aren't levels
     let values = state.split(" "); //save values to a string
     if (values.length > 0) t = parseFloat(values[0]);
     if (values.length > 1) q = parseBigNumber(values[1]);
-    if (values.length > 2) currency.value = parseBigNumber(values[2]);
+    //if (values.length > 2) currency.value = parseBigNumber(values[2]);
 }
 
 var getInternalState = () => {
     resetToPIMult();
     currency.value = 0;
-    return `${t} ${q} ${currency.value}` //return the data saved 
+    return `${t} ${q}`// ${currency.value}` //return the data saved 
 }
 init();
