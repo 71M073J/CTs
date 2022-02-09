5b6e12e8c471f4765e7021004a4ce7cc31db4d38
71M073J <tc7476@student.uni-lj.si>
2022-02-09 21:08:51 +0100

    balance and last changes

--- SinusoidalTheory.js
+++ SinusoidalTheory.js
@@ -1,4 +1,4 @@
-﻿﻿import { ConstantCost, Cost, CustomCost, ExponentialCost, FreeCost, LinearCost, StepwiseCost } from "./api/Costs";
+﻿﻿import { CompositeCost, ConstantCost, Cost, CustomCost, ExponentialCost, FreeCost, LinearCost, StepwiseCost } from "./api/Costs";
 import { Localization } from "./api/Localization";
 import { BigNumber, parseBigNumber } from "./api/BigNumber";
 import { theory } from "./api/Theory";
@@ -15,20 +15,16 @@ var lastTickWasAFK = false;
 var currency;
 var f, t, c, c1, c2, q, q1, p, dt, unbreak;
 var dtMilestone, qPowMilestone, qMilestone;
-var dts = [0.1, 0.025, 6.25e-3];
 var achievement1, achievement2;
 //var chapter1, chapter2;
-var maxt;
-var maxCurrVal;
-var ups;
-var buys;
+var maxt, currMax, ups, buys, taupau = 0.2;
 var init = () => {
     currency = theory.createCurrency();
     //currency2 = theory.createCurrency();
     t = BigNumber.ZERO;
     c = BigNumber.ONE;
     maxt = BigNumber.ZERO;
-    maxCurrVal = BigNumber.ZERO;
+    currMax = 1;
     q = BigNumber.ONE;
     buys = 0;
     max = (a,b) => {
@@ -45,8 +41,8 @@ var init = () => {
     };
     ups = [
         new FreeCost(),
-        new ExponentialCost(1e10, Math.sqrt(3)),
-        new ExponentialCost(1e20, 30), 
+        new ExponentialCost(1e25, Math.sqrt(5)),
+        new ExponentialCost(1e50, 35), 
         new ExponentialCost(1, 2), 
         new ExponentialCost(1, Math.log10(3)), 
         new ExponentialCost(50, Math.sqrt(5))
@@ -96,6 +92,7 @@ var init = () => {
         p.getDescription = (_) => Utils.getMath(getDesc(p.level));
         p.getInfo = (amount) => Utils.getMathTo(getInfo(p.level), getInfo(p.level + amount));
         p.boughtOrRefunded = (_) => resetToPIMult();
+        
     }
     // c1
     
@@ -140,13 +137,13 @@ var init = () => {
     }*/
     ///////////////////////
     //// Milestone Upgrades
-    theory.setMilestoneCost(new LinearCost(25, 25));
-
+    theory.setMilestoneCost(new CompositeCost(7, new LinearCost(25/**taupau*/, 25/**taupau*/), new LinearCost(200/**taupau*/, 50/**taupau*/)));
+    
     //dt milestone
     {
         dtMilestone = theory.createMilestoneUpgrade(0, 2);
-        dtMilestone.description = Localization.getUpgradeMultCustomDesc("dt", "0.25") + " " + Localization.getUpgradeMultCustomDesc("p", "\\sqrt{2}") + ", t = sqrt(t)";
-        dtMilestone.info = Localization.getUpgradeMultCustomInfo("dt", "0.25") + " " + Localization.getUpgradeMultCustomInfo("p", "\\sqrt{2}") + ", t = sqrt(t)";
+        dtMilestone.description = Localization.getUpgradeMultCustomDesc("p", "\\sqrt{2}") + ", t = sqrt(t)";
+        dtMilestone.info = Localization.getUpgradeMultCustomInfo("p", "\\sqrt{2}") + ", t = sqrt(t)";
         dtMilestone.bought = (_) => {theory.invalidatePrimaryEquation(); t = t.sqrt(); maxt = t; resetToPIMult();};
         dtMilestone.isAvailable = true;
     }
@@ -166,10 +163,17 @@ var init = () => {
         qPowMilestone = theory.createMilestoneUpgrade(2, 3);
         qPowMilestone.description =  Localization.getUpgradeIncCustomExpDesc("q", "0.05");
         qPowMilestone.info = Localization.getUpgradeIncCustomExpInfo("q", "0.05");
-        qPowMilestone.boughtOrRefunded = (_) => {theory.invalidatePrimaryEquation(); updateAvailability();};
+        qPowMilestone.boughtOrRefunded = (_) => {theory.invalidatePrimaryEquation(); updateAvailability(); resetToPIMult(); currency.value = BigNumber.ZERO;};
         qPowMilestone.isAvailable = false;
     }
-    //TODO q power exponent i.e. q˘1.15nshiet
+    //c power milestone
+    {
+        cPowMilestone = theory.createMilestoneUpgrade(3, 5);
+        cPowMilestone.description =  Localization.getUpgradeIncCustomExpDesc("c", "0.001");
+        cPowMilestone.info = Localization.getUpgradeIncCustomExpInfo("c", "0.001");
+        cPowMilestone.boughtOrRefunded = (_) => {theory.invalidatePrimaryEquation(); updateAvailability(); resetToPIMult(); currency.value = BigNumber.ZERO;};
+        cPowMilestone.isAvailable = false;
+    }
     /////////////////
     //// Achievements
     //TODO some time in the future
@@ -182,7 +186,19 @@ var updateAvailability = () => {
     q1.isAvailable = qMilestone.level > 0;
     q2.isAvailable = qMilestone.level > 1;
     qPowMilestone.isAvailable = qMilestone.level > 0;
+    cPowMilestone.isAvailable = qPowMilestone.level > 2;
 }
+
+var simulateTResets = (elapsedTime) => {
+    buys = 0;
+    for(let i = 1; i < theory.upgrades.length; i ++){
+        let upg = theory.upgrades[i];
+        buys += ups[i].getMax(upg.level, max(currency.value, 1));
+        theory.upgrades[i].buy(-1);
+    }
+    t = max(max(t - (t % (2 * Math.PI)), maxt), t + (getdt() * elapsedTime * 10) - (buys * (Math.PI / 2)));
+}
+
 
 var tick = (elapsedTime, multiplier) => {
     let dt = BigNumber.from(elapsedTime * multiplier);
@@ -191,26 +207,22 @@ var tick = (elapsedTime, multiplier) => {
         bonus * 
         (qMilestone.level > 0 ? q.pow(qPowMilestone.level * 0.05 + 1) : 1) *
         (getF(f.level) + 
-        c *
-        (t.pow(Math.pow(Math.sqrt(2), qMilestone.level) * getP(p.level)) /  (10*dts[dtMilestone.level])) *
+        (cPowMilestone.level > 0 ? c.pow(cPowMilestone.level * 0.001 + 1) : c) *
+        (t.pow(Math.pow(Math.sqrt(2), qMilestone.level) * getP(p.level)) /  (10*getdt())) *
         Math.cos(t.toNumber()));// - Math.sin(t) + Math.cos(t)) //.pow(getC2Exponent(c2Exp.level))
     
     
-    q += ((getQ1(q1.level) * getQ2(q2.level)) / 1e3) * (elapsedTime * 10);
-    c += (getC1(c1.level) * getC2(c2.level));
+    q += ((getQ1(q1.level) * getQ2(q2.level)) / 1e5) * (elapsedTime * 10);
+    c += (getC1(c1.level) * getC2(c2.level) * getdt());
     //buys = ups[3].getMax(p.level, currency.value)
-
-    //maxCurrVal = currency.value > maxCurrVal ? currency.value : maxCurrVal;
+    if(currency.value > 0 && currency.value.log10() > currMax){
+        currMax = currency.value.log10().toNumber();
+    }
     if(game.isCalculatingOfflineProgress){
         if(theory.isAutoBuyerActive && currency.value > 0){
-            buys = 0;
-            for(let i = 1; i < theory.upgrades.length; i ++){
-                let upg = theory.upgrades[i];
-                buys += ups[i].getMax(upg.level, max(currency.value, 1));
-            }
-            t = max(max(t - (t % (2 * Math.PI)), maxt), t + (dts[dtMilestone.level] * elapsedTime * 10) - buys * (Math.PI / 2))
+            simulateTResets(elapsedTime)
         }else{
-            t += dts[dtMilestone.level] * elapsedTime * 10
+            t += getdt() * elapsedTime * 10
         }
         lastTickWasAFK = true;
     }else{
@@ -222,20 +234,21 @@ var tick = (elapsedTime, multiplier) => {
             currency.value = BigNumber.ZERO;
             lastTickWasAFK = false;        
         }
-        if(elapsedTime > 0.5 && theory.isAutoBuyerActive){
-
-            buys = 0;
-            for(let i = 0; i < theory.upgrades.length; i ++){
-                let upg = theory.upgrades[i];
-                if (i == 0) continue;
-                buys += ups[i].getMax(upg.level, max(currency.value, 1));
-
+        if(elapsedTime > 0.5){
+            if(theory.isAutoBuyerActive){
+                simulateTResets(elapsedTime);
+                resetToPIMult();
+                currency.value = BigNumber.ZERO;
+            }else{
+                simulateTResets(elapsedTime);
+                //resetToPIMult();
+                //currency.value = BigNumber.ZERO;
             }
-            t = max(max(t - (t % (2 * Math.PI)), maxt), t + (dts[dtMilestone.level] * elapsedTime * 10) - buys * 2 * Math.PI)
-            resetToPIMult();
-            currency.value = BigNumber.ZERO;
+            
+            //resetToPIMult();
+            //currency.value = BigNumber.ZERO;
         }else{
-            t += dts[dtMilestone.level] * elapsedTime * 10
+            t += getdt() * elapsedTime * 10
         }
     }
     theory.invalidateTertiaryEquation();
@@ -244,7 +257,7 @@ var tick = (elapsedTime, multiplier) => {
 
 var getPrimaryEquation = () => {
     let result = "\\dot\\rho = f + c";
-
+    result += cPowMilestone.level > 0 ? "^{" + (cPowMilestone.level * 0.001 + 1) + "}" : ""
     //if (c1Exp.level == 1) result += "^{0.05}";
     //if (c1Exp.level == 2) result += "^{0.1}";
     //if (c1Exp.level == 3) result += "^{0.15}";
@@ -269,19 +282,20 @@ var postPublish = () => {
     maxt = BigNumber.ZERO;
     q = BigNumber.ONE;
     c = BigNumber.ONE;
+    currMax = 1;
 }
 var getSecondaryEquation = () => {
     
     let result = "\\begin{matrix}";
     result += "c = {" + c + "} \\ "
-    result += "\\dot c=c_1c_2"
+    result += "\\dot c=c_1c_2dt"
     if(qMilestone.level > 0){
         result += "\\\\";
         result += "q={" + q + "}";
         result += "\\ ";
         result += "\\dot q={q_1" 
         result += qMilestone.level > 1 ? "q_2" : ""
-        result += "}/1e3";
+        result += "}/1e5";
     }
     //result += "\\\\"
     //result += "buys" + buys
@@ -293,19 +307,21 @@ var getTertiaryEquation = () => {
     result += "\\ \\ "
     result += "t={" + t.toNumber().toFixed(2) + "}";
     result += "\\ ";
-    result += "dt=" + dts[dtMilestone.level];
+    result += "dt=" + getdt().toFixed(2);
+    //result += "dt" + Math.pow(1/currMax, 0.75);
     return result;
 }
-var getPublicationMultiplier = (tau) => tau.pow(0.1) * 10;
+var getPublicationMultiplier = (tau) => tau.pow(0.1) / 10;
 var getPublicationMultiplierFormula = (symbol) => "10 \\cdot" + symbol + "^{0.1}";
-var getTau = () => currency.value;
+var getTau = () => currency.value.abs();//.pow(taupau);
 var get2DGraphValue = () => currency.value.sign * (BigNumber.ONE + currency.value.abs()).log10().toNumber();
-var getF = (level) => (level * 100)/1000
+var getF = (level) => (level * 100)/1000;
 var getC1 = (level) => Utils.getStepwisePowerSum(level, 2, 10, 1);
 var getC2 = (level) => BigNumber.TWO.pow(level);
-var getQ1 = (level) => Utils.getStepwisePowerSum(level, 2, 20, 1) //BigNumber.from(level);
+var getQ1 = (level) => Utils.getStepwisePowerSum(level, 2, 15, 1) //BigNumber.from(level);
 var getQ2 = (level) => BigNumber.THREE.pow(level);
-var getP = (level) => BigNumber.from(1 + (level / 100)) 
+var getP = (level) => BigNumber.from(1 + (level / 100)) ;
+var getdt = () => min(0.1, Math.pow(1/currMax, 0.75));
 var getC1Exponent = (level) => BigNumber.from(1 + 0.05 * level);
 var getC2Exponent = (level) => BigNumber.from(1 + 0.05 * level);
 //if you just need to serialise "t", here's an example: quoth philles gillipe
@@ -314,12 +330,13 @@ var setInternalState = (state) => { //set the internal state of values that need
     if (values.length > 0) t = parseBigNumber(values[0]);
     if (values.length > 1) q = parseBigNumber(values[1]);
     if (values.length > 2) c = parseBigNumber(values[2]);
+    if (values.length > 3) currMax = parseFloat(values[3]);
     //if (values.length > 2) currency.value = parseBigNumber(values[2]);
 }
 
 var getInternalState = () => {
     //resetToPIMult();
     //currency.value = 0;
-    return `${t} ${q} ${c}`// ${currency.value}` //return the data saved 
+    return `${t} ${q} ${c} ${currMax}`// ${currency.value}` //return the data saved 
 }
 init();
